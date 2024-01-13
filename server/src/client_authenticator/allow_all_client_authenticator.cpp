735448f4056ba8e61191262c03e6f6901101fcd0
Rafael Fillipe Silva <rfsc.mori@gmail.com>
2024-01-13 13:28:01 +0000

    feat: implement a client authenticator that assumes all usernames and passwords are valid
    Signed-off-by: Rafael Fillipe Silva <rfsc.mori@gmail.com>

---
+++ server/src/client_authenticator/allow_all_client_authenticator.cpp
@@ -0,0 +1,19 @@
+#include "client_authenticator/allow_all_client_authenticator.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace mori_echo::auth {
+
+[[nodiscard]] auto logger() -> std::shared_ptr<spdlog::logger> {
+  static auto logger =
+      spdlog::default_logger()->clone("allow_all_client_authenticator");
+  return logger;
+}
+
+auto allow_all_client_authenticator::authenticate(
+    std::string_view username, [[maybe_unused]] std::string_view password)
+    -> void {
+  logger()->info("Authenticated client: {}", username);
+}
+
+} // namespace mori_echo::auth
