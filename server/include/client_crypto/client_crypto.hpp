23717407e665ea05f05bbce51035bd788c7194be
Rafael Fillipe Silva <rfsc.mori@gmail.com>
2024-01-14 18:59:02 +0000

    refactor: update parameter name to match with implementation
    Signed-off-by: Rafael Fillipe Silva <rfsc.mori@gmail.com>

--- server/include/client_crypto/client_crypto.hpp
+++ server/include/client_crypto/client_crypto.hpp
@@ -8,7 +8,7 @@
 
 namespace mori_echo::crypto {
 
-auto calculate_checksum(std::string_view message) -> std::uint8_t;
+auto calculate_checksum(std::string_view data) -> std::uint8_t;
 
 auto decrypt(crypto_message_params args, std::vector<std::byte> message)
     -> std::vector<std::byte>;
